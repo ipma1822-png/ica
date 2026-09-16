3f91132d5144228cba2803aecc28934b5333648e
IPMA Global <ipma.1822@gmail.com>
2026-09-16 16:19:27 +0900

    Build ICA global credential system v1.0

---
+++ assets/js/ica-config.js
@@ -0,0 +1,5 @@
+window.ICA_CONFIG = Object.freeze({
+  supabaseUrl: 'https://ojxarsfaewehwjidwgac.supabase.co',
+  supabaseKey: 'sb_publishable_ZoAZrV5rDmYDLxhXlnEXCw_lPqJfin0',
+  verificationUrl: 'https://ipma1822-png.github.io/ica/verification/'
+});
